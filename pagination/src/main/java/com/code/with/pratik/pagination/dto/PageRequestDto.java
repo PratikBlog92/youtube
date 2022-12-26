6f9c3154f175cf67fffb129da000b783ae7dacfd
Pratik Dandare <pgdandare@gmail.com>
2022-12-26 22:32:36 +0530

    added sort functionality

--- pagination/src/main/java/com/code/with/pratik/pagination/dto/PageRequestDto.java
+++ pagination/src/main/java/com/code/with/pratik/pagination/dto/PageRequestDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Objects;
 
@@ -13,12 +14,20 @@ public class PageRequestDto {
 
     private Integer pageNo = 0;
     private Integer pageSize = 10;
+    private Sort.Direction sort = Sort.Direction.ASC;
+
+    private String sortByColumn = "id";
 
     public Pageable getPageable(PageRequestDto dto){
         Integer page = Objects.nonNull(dto.getPageNo()) ? dto.getPageNo() : this.pageNo;
         Integer size = Objects.nonNull(dto.getPageSize()) ? dto.getPageSize() : this.pageSize;
+        Sort.Direction sort = Objects.nonNull(dto.getSort()) ? dto.getSort() : this.sort;
+        String sortByColumn = Objects.nonNull(dto.getSortByColumn()) ? dto.getSortByColumn() : this.sortByColumn;
 
-        PageRequest request = PageRequest.of(page, size);
+
+//        PageRequest request = PageRequest.of(page, size);
+        PageRequest request = PageRequest.of(page, size, sort, sortByColumn);
+
         return request;
     }
 
