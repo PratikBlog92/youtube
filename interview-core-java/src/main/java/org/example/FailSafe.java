09d20e0f3882d62966d3dc0758bc1aac3b6aa9d5
Pratik Dandare <pgdandare@gmail.com>
2025-01-16 14:28:51 +0530

    modified for reels

--- interview-core-java/src/main/java/org/example/FailSafe.java
+++ interview-core-java/src/main/java/org/example/FailSafe.java
@@ -12,14 +12,17 @@ public class FailSafe {
 
 //    The key benefit provided by a fail-safe iterator is the
 //    fact that they minimize the possibility of exceptions being thrown.
+
 //    But that benefit comes with a cost, one of which is increased memory requirement
 
-    //    Disadvantages
+//    Disadvantages
 //    1. Iterator isn’t guaranteed to return updated data from the Collection,
 //      as it’s working on the clone instead of the actual Collection
 //    2. It consumes more memory and time
+
     public static void main(String[] args) {
-        List<String> fruits = new CopyOnWriteArrayList<>();
+        List<String> fruits =
+                new CopyOnWriteArrayList<>();
         fruits.add("apple");
         fruits.add("banana");
         fruits.add("coconut");
