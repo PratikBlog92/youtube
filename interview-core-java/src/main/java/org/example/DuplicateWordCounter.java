649369a188f66e5f393357b05ad022ccd42d42f8
Pratik Dandare <pgdandare@gmail.com>
2025-01-20 16:32:16 +0530

    added duplicate word counter program

--- interview-core-java/src/main/java/org/example/DuplicateWordCounter.java
+++ interview-core-java/src/main/java/org/example/DuplicateWordCounter.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class DuplicateWordCounter {
     public static void main(String[] args) {
 //   Q. Java program to count duplicate words in a string
-        String input = "my name is pratik. He love programming." +
-                " He currently writting" +
+
+        String input = "my name is pratik. He love programming...!" +
+                " He currently writting, " +
                 "Java program to count duplicate words in a string ";
 
         String cleanedInput = input.toLowerCase()
@@ -34,28 +32,28 @@ public class DuplicateWordCounter {
 
 
         // TODO Way 2
-        for (String word : words) {
-            wordCountMap.put(word,
-                    wordCountMap.getOrDefault(word, 0) + 1);
-        }
-        System.out.println(wordCountMap);
+//        for (String word : words) {
+//            wordCountMap.put(word,
+//                    wordCountMap.getOrDefault(word, 0) + 1);
+//        }
+//        System.out.println(wordCountMap);
 
 
         //TODO way 3
-        Map<String, Long> wordCount = Arrays.stream(
-                cleanedInput.split("\\s+"))
-                .collect(Collectors.groupingBy
-                        (Function.identity(), Collectors.counting()));
-
-        System.out.println(wordCount);
+//        Map<String, Long> wordCount = Arrays.stream(
+//                cleanedInput.split("\\s+"))
+//                .collect(Collectors.groupingBy
+//                        (Function.identity(), Collectors.counting()));
+//
+//        System.out.println(wordCount);
 
 
         //TODO way 4 convert it to integer
-        wordCountMap = Arrays.stream(cleanedInput.split("\\s+"))
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.collectingAndThen(
-                                Collectors.counting(), Long::intValue)
-                ));
+//        wordCountMap = Arrays.stream(cleanedInput.split("\\s+"))
+//                .collect(Collectors.groupingBy(
+//                        Function.identity(),
+//                        Collectors.collectingAndThen(
+//                                Collectors.counting(), Long::intValue)
+//                ));
     }
 }
